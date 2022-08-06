8758bbfd6cefd13286baed6698d27d5920697070
Vitor Sanches <viisanches98@gmail.com>
2022-08-06 01:12:35 -0300

    fix: correçoes na apresentaçao

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import { FilmAgent } from './agents/film-agent';
-import { User } from './models/user';
 import { UserAgent } from './agents/user-agent';
 import initializeDatabase from './database/neo4j.driver';
 
@@ -12,48 +11,69 @@ const main = async () => {
   const userAgent = new UserAgent(driver);
   const filmAgent = new FilmAgent(driver);
 
-  const julio = await userAgent.create({ name: 'Julio' });
-  const renato = await userAgent.create({ name: 'Renato' });
-  //const marcos = await userAgent.create({ name: 'Marcos' });
-  const estefano = await userAgent.create({ name: 'Estefano' });
+  // Criamos nosso usuário
+  const fabio = await userAgent.create({ name: 'Fabio' });
 
-  const recommendations = filmAgent.findRecommendations(julio);
+  // Filmes que fabio gosta
+  await userAgent.likeFilm(fabio, 121);
+  await userAgent.likeFilm(fabio, 116);
+  await userAgent.likeFilm(fabio, 111);
 
-  await userAgent.becomeFriendsWith({ user: julio, friend: renato });
-  // await userAgent.becomeFriendsWith({ user: julio, friend: marcos });
-  await userAgent.becomeFriendsWith({ user: julio, friend: estefano });
+  // Encontraremos as recomendações base de Fabio
+  const baseRecommendations = await filmAgent.findRecommendations(fabio);
 
-  // Filmes que Julio gosta
-  await userAgent.likeFilm(julio, 1);
-  await userAgent.likeFilm(julio, 2);
-  await userAgent.likeFilm(julio, 3);
+  // Exibimos em tela
+  const jsonBaseRecommendations = baseRecommendations.map((film) => film.toJSON());
+  console.log('\n\nRECOMENDAÇÕES INICIAIS\n')
+  console.log(JSON.stringify(jsonBaseRecommendations, null, 2));
 
-  // Filmes que Renato gosta
-  await userAgent.likeFilm(julio, 4);
-  await userAgent.likeFilm(julio, 5);
-  await userAgent.likeFilm(julio, 6);
+  // Criaremos mais alguns usuários
+  const marcos = await userAgent.create({ name: 'Marcos' });
+  const luiz = await userAgent.create({ name: 'Luiz' });
+  const antonio = await userAgent.create({ name: 'Antonio' });
+
+  // Relacionaremos eles com o Fabio
+  await userAgent.becomeFriendsWith({ user: fabio, friend: marcos });
+  await userAgent.becomeFriendsWith({ user: fabio, friend: luiz });
+  await userAgent.becomeFriendsWith({ user: fabio, friend: antonio });
 
   // Filmes que Marcos gosta
-  await userAgent.likeFilm(julio, 5);
-  await userAgent.likeFilm(julio, 6);
-  await userAgent.likeFilm(julio, 7);
+  await userAgent.likeFilm(marcos, 152);
+  await userAgent.likeFilm(marcos, 154);
+  await userAgent.likeFilm(marcos, 157);
 
-  // Filmes que Estefano gosta
-  await userAgent.likeFilm(julio, 5);
-  await userAgent.likeFilm(julio, 7);
-  await userAgent.likeFilm(julio, 8);
+  // Filmes que Luiz gosta
+  await userAgent.likeFilm(luiz, 161);
+  await userAgent.likeFilm(luiz, 0);
+  await userAgent.likeFilm(luiz, 11);
 
-  const julioRecommendations = await filmAgent.findRecommendations(julio);
+  // Filmes que Antonio gosta
+  await userAgent.likeFilm(antonio, 11);
+  await userAgent.likeFilm(antonio, 152);
+  await userAgent.likeFilm(antonio, 56);
 
-  const jsonRecommendations = julioRecommendations.map((film) => film.toJSON());
+  // Executar a seguinte query no browser do Neo4j:
+  // MATCH (m:Movie)<-[:LIKES]-(u:User)-[:FRIENDS_WITH]->(u2:User)-[:LIKES]->(m2:Movie)
+  // WHERE u.name = 'Fabio'
+  // RETURN u, m, u2, m2
 
-  // const marcos = await userAgent.findUserById(178);
-  //const teste = marcos.toJSON();
+  const fabioRecommendations = await filmAgent.findRecommendations(fabio);
 
-  // console.log(marcos.identity.low);
+  const jsonRecommendations = fabioRecommendations.map((film) => film.toJSON());
 
-  /* console.log('RECOMENDAÇÕES PARA JULIO')
-  console.log(JSON.stringify(jsonRecommendations, null, 2)); */
+  console.log('\n\nRECOMENDAÇÕES FINAIS\n')
+  console.log(JSON.stringify(jsonRecommendations, null, 2));
 }
 
-main();
+const down = async () => {
+  const driver = await initializeDatabase();
+
+  await driver.session().run(`MATCH (a: User) WHERE a.name='Marcos' DETACH DELETE a`);
+  await driver.session().run(`MATCH (a: User) WHERE a.name='Luiz' DETACH DELETE a`);
+  await driver.session().run(`MATCH (a: User) WHERE a.name='Antonio' DETACH DELETE a`);
+  await driver.session().run(`MATCH (a: User) WHERE a.name='Fabio' DETACH DELETE a`);
+
+  console.log('pronto');
+}
+
+down();
--- src/tests/queries-apresentacao.ts
+++ src/tests/queries-apresentacao.ts
@@ -1,6 +1,6 @@
 const queryCompleta = `
   CALL {
-    MATCH (u:User)-[r:FRIENDS_WITH]->(f:User)-[:LIKES]->(m:Movie)
+    MATCH (u:User)-[r:FRIENDS_WITH]-(f:User)-[:LIKES]->(m:Movie)
     WHERE u.name = 'Julio'
     RETURN m
 
@@ -22,13 +22,18 @@ const queryCompleta = `
 `;
 
 const queryAmigos = `
-  MATCH (u:User)-[r:FRIENDS_WITH]->(f:User)-[:LIKES]->(m:Movie)
+  MATCH (u:User)-[r:FRIENDS_WITH]-(f:User)-[:LIKES]->(m:Movie)
   WHERE u.name = 'Julio'
   RETURN m, count(m) AS occurrence
   ORDER BY occurrence DESC
   LIMIT 10
 `
 
+// Para exemplo do documento
+// MATCH (u:User)-[:LIKES]->(m1:Movie)<-[]-(p:Person)-[]->(m:Movie)
+// WHERE u.name = 'Julio'
+// RETURN m, m1, u, p
+// LIMIT 20
 const queryPerson = `
   MATCH (u:User)-[:LIKES]->(m1:Movie)<-[]-(p:Person)-[]->(m:Movie)
   WHERE u.name = 'Julio'
@@ -37,7 +42,10 @@ const queryPerson = `
   LIMIT 10
 `;
 
-
+// Para exemplo do Documento
+// MATCH (u:User)-[:LIKES]->(m1:Movie)-[]-(g:Genre)-[]-(m:Movie)
+// WHERE u.name = 'Julio'
+// RETURN m, m1, u, g
 const queryGenre = `
   MATCH (u:User)-[:LIKES]->(m1:Movie)<-[]-(g:Genre)-[]->(m:Movie)
   WHERE u.name = 'Julio'
